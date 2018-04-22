d952f885a40a3cfff36ecd1dff088c701df90cd4
Kevin Weispfennig <ghorthalon@MacBook-Air-2.local>
2018-04-22 19:27:23 +0200

    ixed double opening bug for settings and online list

---
+++ js/ui/onlinelist.js
@@ -0,0 +1,104 @@
+'use strict';
+import "./ui.css";
+
+const React = require("react");
+
+import {
+	Accordion,
+	AccordionItem,
+	AccordionItemTitle,
+	AccordionItemBody
+} from 'react-accessible-accordion';
+
+class OnlineList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			people: {
+				active:[],
+				idlers:[],
+				bots:[]
+			}
+			
+		}
+		this.people = {
+			active: [],
+			bots: [],
+			idlers: []
+		}
+
+		this.createGroup = this.createGroup.bind(this);
+		
+			this.response = null;
+			
+	}
+	
+	componentWillMount() {
+		this.getOnlineList();
+	}
+	
+	getOnlineList() {
+		fetch("http://chatmud.com/api/who")
+		.then(response => response.json())
+		.then(data => this.parseData(data));
+	}
+	
+	parseData(data) {
+		for (const item in data.list) {
+			console.log("Parsing item: " + JSON.stringify(data.list[item]));
+			
+			if (data.list[item].flags && data.list[item].flags.includes("bot")) {
+				const person = {
+					name:item,
+					title:data.list[item].title
+				}
+				this.people.bots.push(person);
+			} else if (data.list[item].flags.includes("idle")) {
+				const person = {
+					name:item,
+					title:data.list[item].title
+				}
+				this.people.idlers.push(person);
+			} else {
+				const person = {
+					name:item,
+					title:data.list[item].title
+				}
+				this.people.active.push(person);
+			}
+			
+		}
+		this.setState({
+			people:this.people
+		});
+		
+	}
+	
+	createGroup(title, people) {
+		return (
+			<div>
+			<h2>{title}</h2>
+			<ul>
+			{people.map((item, index) => {
+				console.log("Mapping " + item.name);
+				return <li>{item.name} - {item.title}</li>
+			})}
+			</ul>
+			</div>
+		);
+	}
+	
+	render() {
+		return (
+		<div class="settings-panel">
+		<h1>Who's online</h1>
+		{this.createGroup("Active", this.state.people.active)}
+		{this.createGroup("Bots", this.state.people.bots)}
+		{this.createGroup("Idlers", this.state.people.idlers)}
+		</div>
+		);
+	}
+	
+}
+
+module.exports = OnlineList;
--- js/ui/toolbar.js
+++ js/ui/toolbar.js
@@ -2,49 +2,112 @@ import './ui.css';
 
 const React = require('react');
 const SettingsPanel = require('./settingspanel');
+const OnlineList = require("./onlinelist");
 
 class ToolBar extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			isOpened: false
+			isSettingsOpened: false,
+			isOnlineListOpened: false
 		};
 		this.showSettings = this.showSettings.bind(this);
 		this.hideSettings = this.hideSettings.bind(this);
+		this.showOnlineList = this.showOnlineList.bind(this);
+		this.hideOnlineList = this.hideOnlineList.bind(this);
 	}
 
 	render() {
-		if (this.state.isOpened) {
-			return (
-				<div>
-					<div class="toolbar">
-						<button onClick={this.hideSettings} aria-expanded="true">Settings</button>
+					return (
+					<div>
+					{this.renderButtons()}
+					{this.renderSettings()}
+					{this.renderOnlineList()}
 					</div>
+					);
+	}
+	
+	showSettings() {
+		this.setState({
+			isSettingsOpened: true,
+			isOnlineListOpened:false
+		});
+	}
+
+	hideSettings() {
+		this.setState({
+			isSettingsOpened: false,
+			isOnlineListOpened:false
+		});
+	}
+	
+	renderButtons() {
+		let buttons = [];
+
+			if (this.state.isSettingsOpened == true) { 
+				buttons.push(<button onClick={this.hideSettings} aria-expanded="true">Settings</button>);
+			} else {
+				buttons.push(				<button onClick={this.showSettings} aria-expanded="false">Settings</button>);
+			}
+			
+		if (this.state.isOnlineListOpened) {
+									buttons.push(<button onClick={this.hideOnlineList} aria-expanded="true">Online List</button>);
+		} else {
+			buttons.push(				<button onClick={this.showOnlineList} aria-expanded="false">Online List</button>);
+		}
+		return (
+		<div class="toolbar">
+		{buttons}
+		</div>
+		);
+
+	}
+	
+	showOnlineList() {
+		this.setState({
+			isOnlineListOpened: true,
+			isSettingsOpened:false
+		});
+	}
+
+	hideOnlineList() {
+		this.setState({
+			isOnlineListOpened: false,
+			isSettingsOpened:false
+		});
+	}
+
+	renderSettings() {
+		if (this.state.isSettingsOpened) {
+return (
+				<div>
+
 					<div class="settings-panel">
 						<SettingsPanel instance={this.props.instance}/>
 					</div>
 
 				</div>
 			);
-		}
-		return (
-			<div class="toolbar">
-				<button onClick={this.showSettings} aria-expanded="false">Settings</button>
-			</div>
-		);
-	}
+		}		
+		
 
-	showSettings() {
-		this.setState({
-			isOpened: true
-		});
-	}
 
-	hideSettings() {
-		this.setState({
-			isOpened: false
-		});
 	}
+	
+	renderOnlineList() {
+		if (this.state.isOnlineListOpened) {
+return (
+				<div>
+
+					<div class="settings-panel">
+						<OnlineList />
+					</div>
+
+				</div>
+			);
+		}		
+	}
+	
 }
 
 module.exports = ToolBar;
