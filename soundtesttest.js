4f80784b87968bf0c5f2fdc17d0906addf937cbc
Talon <talon@iamtalon.me>
2018-10-23 15:03:56 +0200

    Cleaning up project directory structure

--- soundtesttest.js
+++
@@ -1,5 +0,0 @@
-const soundops = require("./soundtest");
-
-let socials = soundops.findSoundsInFolder("socials");
-let kicks = soundops.findFilenames("Kick", socials);
-console.log("Found: " + JSON.stringify(kicks));
