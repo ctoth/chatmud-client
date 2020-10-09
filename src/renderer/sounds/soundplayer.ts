c8b46470fd02c52fd19583c9292a90bf6a402ed8
Christopher Toth <q.alpha@gmail.com>
2020-10-09 10:24:29 -0600

    More fixes to sounds and started typescripting

--- src/renderer/sounds/soundplayer.ts
+++ src/renderer/sounds/soundplayer.ts
@@ -1,67 +1,16 @@
-import { Howl, Howler } from 'howler';
+import { Howl } from 'howler';
 import rng from 'random-int';
 import soundops from './soundops';
 import Sounds from './sounds';
 import path from 'path';
-
-class SoundPlayer {
-  constructor(extension = '.m4a') {
-    this.sounds = [];
-    this.extension = extension;
-  }
-
-  play(file, folder = '') {
-    let mFile = this.searchSounds(file, folder);
-    if (mFile == -1) {
-      mFile = this.loadSound(file, folder);
-    }
-    mFile.sound.play();
-  }
-
-  playChannel(name) {
-    const channels = soundops.findSoundsInFolder('channels');
-    const foundChannels = soundops.findFilenames(name, channels);
-    name = foundChannels.length == 0 ? 'global' : name;
-    this.play(name, 'channels');
-  }
-
-  playSocial(name, gender) {
-    const socials = soundops.findSoundsInFolder('socials');
-    let foundSocials = soundops.findFilenames(name + '_' + gender, socials);
-    if (foundSocials.length == 0) {
-      foundSocials = soundops.findFilenames(name, socials);
-    }
-    let filename = null;
-    if (foundSocials.length > 0) {
-      filename = foundSocials[rng(0, foundSocials.length - 1)].toString();
-      // filename = filename.slice(0, filename.length - 4);
-    }
-
-    if (!filename) {
-      return;
-    }
-
-    this.play(filename, 'socials');
-  }
-
-  searchSounds(file, folder) {
-    for (const sound of this.sounds) {
-      if (sound.file == file && sound.folder == folder) {
-        return sound;
-      }
-    }
-    return -1;
-  }
-
-  loadSound(file, folder) {
-    const sound = new Sound(folder, file);
-    this.sounds.push(sound);
-    return sound;
-  }
-}
-
-class Sound {
-  constructor(folder, file, extension = '.m4a') {
+export class Sound {
+  basePath: string;
+  file: string;
+  folder: string;
+  extension: string;
+  path: string;
+  sound: Howl;
+  constructor(folder: string, file: string, extension = '.m4a') {
     this.basePath = './sounds/';
     this.file = file;
     this.folder = folder;
@@ -75,4 +24,58 @@ class Sound {
   }
 }
 
-export default SoundPlayer;
+export class SoundPlayer {
+  sounds: Sound[] = [];
+  extension: string;
+  constructor(extension = '.m4a') {
+    this.extension = extension;
+  }
+
+  play(file, folder = ''): void {
+    let mFile = this.searchSounds(file, folder);
+    if (mFile === undefined) {
+      mFile = this.loadSound(file, folder);
+    }
+    mFile.sound.play();
+  }
+
+  playChannel(name) {
+    const channels = soundops.findSoundsInFolder('channels');
+    const foundChannels = soundops.findFilenames(name, channels);
+    name = foundChannels.length === 0 ? 'global' : name;
+    this.play(name, 'channels');
+  }
+
+  playSocial(name, gender) {
+    const socials = soundops.findSoundsInFolder('socials');
+    let foundSocials = soundops.findFilenames(name + '_' + gender, socials);
+    if (foundSocials.length === 0) {
+      foundSocials = soundops.findFilenames(name, socials);
+    }
+    let filename;
+    if (foundSocials.length > 0) {
+      filename = foundSocials[rng(0, foundSocials.length - 1)].toString();
+      // filename = filename.slice(0, filename.length - 4);
+    }
+
+    if (!filename) {
+      return;
+    }
+
+    this.play(filename, 'socials');
+  }
+
+  searchSounds(file: string, folder: string): Sound {
+    for (const sound of this.sounds) {
+      if (sound.file === file && sound.folder === folder) {
+        return sound;
+      }
+    }
+  }
+
+  loadSound(file: string, folder: string): Sound {
+    const sound = new Sound(folder, file);
+    this.sounds.push(sound);
+    return sound;
+  }
+}
