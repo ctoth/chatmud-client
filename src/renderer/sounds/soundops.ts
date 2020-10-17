c27c6e456fb6e3414886bfb7e6ae9d1588930e6d
Christopher Toth <q.alpha@gmail.com>
2020-10-16 19:13:34 -0600

    Fix ordering to not use before declaration

--- src/renderer/sounds/soundops.ts
+++ src/renderer/sounds/soundops.ts
@@ -1,14 +1,5 @@
 import sounds from './sounds.json';
 
-function findSoundsInFolder(path) {
-  const split = path.split('/');
-  let directory = sounds;
-  for (const string of split) {
-    directory = search(string, directory);
-  }
-  return directory;
-}
-
 function search(string, object) {
   for (const entry of object) {
     if (entry.name === string) {
@@ -17,7 +8,16 @@ function search(string, object) {
   }
 }
 
-function findFilenames(string, array): string[] {
+export function findSoundsInFolder(path) {
+  const split = path.split('/');
+  let directory = sounds;
+  for (const string of split) {
+    directory = search(string, directory);
+  }
+  return directory;
+}
+
+export function findFilenames(string, array): string[] {
   const returnObject = [];
   for (const entry of array) {
     if (entry.name.includes(string)) {
@@ -26,10 +26,3 @@ function findFilenames(string, array): string[] {
   }
   return returnObject;
 }
-
-export { findSoundsInFolder };
-export { findFilenames };
-export default {
-  findSoundsInFolder,
-  findFilenames,
-};
